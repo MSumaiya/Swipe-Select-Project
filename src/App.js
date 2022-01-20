de6e49732889e1406d5274ab4d3dad31efcce58b
MSumaiya <mirza01.alam@gmail.com>
2022-01-20 22:36:36 +0100

    Installed material UI version 5 for better control over typography, color etc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box pt={3} sx={{ width: "100%"}}>
+        <Typography fontWeight="500" color="primary" variant="h1" component="div" gutterBottom>
+          Swipe Select Project
+        </Typography>
+      </Box>
+    </>
   );
 }
 
