c1af50c34bf6e1c03309388bdb5feab0f66e1bd8
MSumaiya <mirza01.alam@gmail.com>
2022-01-23 23:41:43 +0100

    Paper css style  and responsiveness fix

--- src/components/SwipeComponent/SwipeComponent.js
+++ src/components/SwipeComponent/SwipeComponent.js
@@ -9,17 +9,26 @@ import "swiper/css";
 import "swiper/css/pagination";
 import classes from "./SwipeComponent.module.css";
 import Button from "@mui/material/Button";
+import EuroSymbolIcon from "@mui/icons-material/EuroSymbol";
 
-import SwiperCore, { Keyboard, Navigation, Mousewheel } from "swiper";
+import SwiperCore, {
+  Keyboard,
+  Navigation,
+  Mousewheel,
+  Pagination,
+} from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 // install Swiper modules
-SwiperCore.use([Keyboard, Navigation, Mousewheel]);
+SwiperCore.use([Keyboard, Navigation, Mousewheel, Pagination]);
 
 export default function SwipeComponent() {
   const [clickValue, setClickValue] = useState("");
   /* const [numStyle, setNumStyle] = useState(false); */
-  const [test, setTest] = useState("");
+  const [selectValue, setSelectValue] = useState("");
   const [edit, setEdit] = useState(false);
+  const [display, setDisplay] = useState(true);
 
   useEffect(() => {
     setClickValue(sessionStorage.getItem("key"));
@@ -30,89 +39,189 @@ export default function SwipeComponent() {
   const handleSwiperSlideClick = (event) => {
     event.preventDefault();
     sessionStorage.setItem("key", event.target.innerText);
-    setTest(event.target.innerText);
+    setSelectValue(event.target.innerText);
     /* console.log("numStyle :", !numStyle ) */
     //setEdit(false);
   };
 
   const handleEdit = () => {
     setEdit(true);
+    setDisplay(false);
   };
 
   const updateEdit = () => {
-    setClickValue(setTest);
+    setClickValue(setSelectValue);
     setEdit(false);
-  }
+    setDisplay(true);
+  };
 
   return (
     <>
-      <Box >
-        <Typography
-          sx={{
-            fontSize: { lg: "4rem", md: "3rem", sm: "2.5rem", xs: "2rem" },
-          }}
-        >
-          Current Selection: {clickValue} Sek/month
-        </Typography>
-        <Box mt={2} p={2} sx={{
-          display:"flex",
-          justifyContent: "end",
-          alignItems:"center",
-        }}>
-          <Button onClick={handleEdit} variant="contained">
-            Edit
-          </Button>
-        </Box>
-      </Box>
-      {edit && (
-        <Box
-          sx={{
-            flexGrow: 1,
-            width: { lg: "50%", md: "50%", sm: "80%", xs: "90%" },
-          }}
-        >
-          <Swiper
-            slidesPerView={5}
-            spaceBetween={0}
-            keyboard={{
-              enabled: true,
-            }}
-            navigation={true}
-            mousewheel={true}
-          >
-            {JsonValues.map((value) => (
-              <SwiperSlide key={value.id} onClick={handleSwiperSlideClick}>
-                <Box
-                  m={5}
+      {display && (
+        <Box sx={{ width: { md: "60%", sm: "75%", xs: "90%" } }}>
+          <Paper elevation={3} sx={{ borderLeft: "8px solid #00817a" }}>
+            <Box
+              pl={2}
+              pr={2}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
                     fontSize: {
-                      lg: "4rem",
-                      md: "3rem",
-                      sm: "2.5rem",
-                      xs: "2rem",
+                      lg: "6rem",
+                      md: "5rem",
+                      sm: "4rem",
+                      xs: "3rem",
                     },
+                    fontWeight: "bolder",
+                    color: "#474747" /* 5b5959 */,
                   }}
                 >
-                  {/* {numStyle ? <span className={classes.test1}>{value.num}</span> : <span>{value.num}</span>} */}
-                  <span>{value.num}</span>
+                  {clickValue}
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "start",
+                    alignItems: "center",
+                  }}
+                >
+                  <EuroSymbolIcon
+                    sx={{
+                      fontSize: {
+                        lg: "4rem",
+                        md: "3rem",
+                        sm: "3rem",
+                        xs: "2.5rem",
+                      },
+                      color: "#5b5959",
+                    }}
+                  ></EuroSymbolIcon>
+                  <Typography
+                    sx={{
+                      fontSize: {
+                        lg: "4rem",
+                        md: "3rem",
+                        sm: "3rem",
+                        xs: "2.5rem",
+                      },
+                      color: "#5b5959",
+                    }}
+                  >
+                    /Month
+                  </Typography>
                 </Box>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <Typography variant="h4">You can swipe from above list. After select a value you need to press update button below.</Typography>
-          <Box mt={2} p={2} sx={{
-            display:"flex",
-            justifyContent: "end",
-            alignItems:"center",
-          }}>
-            <Button onClick={updateEdit} variant="contained">
-              Update
-            </Button>
-          </Box>
+              </Box>
+              <Box
+                mt={2}
+                p={2}
+                sx={{
+                  display: "flex",
+                  justifyContent: "end",
+                  alignItems: "center",
+                }}
+              >
+                <Button fullWidth onClick={handleEdit} variant="contained">
+                  <Typography
+                    sx={{
+                      fontSize: {
+                        sm: "2rem",
+                        xs: "1.5rem",
+                      },
+                    }}
+                  >
+                    Edit
+                  </Typography>
+                </Button>
+              </Box>
+            </Box>
+          </Paper>
         </Box>
+      )}
+
+      {edit && (
+        <>
+          <Box
+            sx={{
+              flexGrow: 1,
+              width: { lg: "50%", md: "50%", sm: "80%", xs: "90%" },
+            }}
+          >
+            <Paper elevation={3} sx={{ borderLeft: "8px solid #00817a" }}>
+              <Swiper
+                slidesPerView={5}
+                spaceBetween={0}
+                keyboard={{
+                  enabled: true,
+                }}
+                navigation={true}
+                mousewheel={true}
+                pagination={{
+                  clickable: true,
+                }}
+              >
+                {JsonValues.map((value) => (
+                  <SwiperSlide key={value.id} onClick={handleSwiperSlideClick}>
+                    <Box
+                      m={5}
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: {
+                          lg: "4rem",
+                          md: "3rem",
+                          sm: "2.5rem",
+                          xs: "2rem",
+                        },
+                      }}
+                    >
+                      {/* {numStyle ? <span className={classes.test1}>{value.num}</span> : <span>{value.num}</span>} */}
+                      <span>{value.num}</span>
+                    </Box>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+              <Typography variant="h4">
+                You can swipe from above list. After select a value you need to
+                press update button below.
+              </Typography>
+              <Box
+                  mt={2}
+                  p={2}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "end",
+                    alignItems: "center",
+                  }}
+                >
+                <Button fullWidth onClick={updateEdit} variant="contained">
+                <Typography
+                    sx={{
+                      fontSize: {
+                        sm: "2rem",
+                        xs: "1.5rem",
+                      },
+                    }}
+                  >
+                    Update
+                  </Typography>
+                </Button>
+              </Box>
+            </Paper>
+          </Box>
+        </>
       )}
     </>
   );
