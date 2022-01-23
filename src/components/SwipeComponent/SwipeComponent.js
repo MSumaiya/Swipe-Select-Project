0cd023d06cb81b4f4b56d4ba057506c4140cf786
MSumaiya <mirza01.alam@gmail.com>
2022-01-23 16:32:57 +0100

    Swipe componenent Edit and Update function created

--- src/components/SwipeComponent/SwipeComponent.js
+++ src/components/SwipeComponent/SwipeComponent.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
@@ -7,86 +7,113 @@ import Typography from "@mui/material/Typography";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import classes from './SwipeComponent.module.css'
+import classes from "./SwipeComponent.module.css";
+import Button from "@mui/material/Button";
 
 import SwiperCore, { Keyboard, Navigation, Mousewheel } from "swiper";
 
 // install Swiper modules
-SwiperCore.use([Keyboard,  Navigation, Mousewheel]);
+SwiperCore.use([Keyboard, Navigation, Mousewheel]);
 
 export default function SwipeComponent() {
-
   const [clickValue, setClickValue] = useState("");
-  const [test, setTest] = useState("")
-  const [style, setstyle] = useState(false);
+  /* const [numStyle, setNumStyle] = useState(false); */
+  const [test, setTest] = useState("");
+  const [edit, setEdit] = useState(false);
 
   useEffect(() => {
-    setClickValue(sessionStorage.getItem('key'));  
-      
-  },[test]);
+    setClickValue(sessionStorage.getItem("key"));
+    clickValue && console.log("clickValue:", clickValue);
+    /* numStyle && setNumStyle(!numStyle ) */
+  }, [clickValue]);
 
   const handleSwiperSlideClick = (event) => {
     event.preventDefault();
     sessionStorage.setItem("key", event.target.innerText);
-    setTest(event.target.innerText)
-    setstyle(true)
-  }
+    setTest(event.target.innerText);
+    /* console.log("numStyle :", !numStyle ) */
+    //setEdit(false);
+  };
 
+  const handleEdit = () => {
+    setEdit(true);
+  };
+
+  const updateEdit = () => {
+    setClickValue(setTest);
+    setEdit(false);
+  }
 
   return (
     <>
-
-      <Box> Current Value: {clickValue}</Box>
-      <Box
-        sx={{
-          flexGrow: 1,
-          width: { lg: "50%", md: "50%", sm: "80%", xs: "90%" },
-        }}
-      >
-        <Swiper
-          slidesPerView={5}
-          spaceBetween={0}
-          keyboard={{
-            enabled: true,
+      <Box >
+        <Typography
+          sx={{
+            fontSize: { lg: "4rem", md: "3rem", sm: "2.5rem", xs: "2rem" },
           }}
-          
-          navigation={true}
-          mousewheel={true}
         >
-          {JsonValues.map((value) => (
-            <SwiperSlide key={value.id} onClick={handleSwiperSlideClick}>
-              <Box
-               
-                m={5}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  fontSize: {
-                    lg: "4rem",
-                    md: "3rem",
-                    sm: "2.5rem",
-                    xs: "2rem",
-                  },
-                  backgroundColor: "primary.dark",
-                  /* "&:hover": {
-                    backgroundColor: "primary.main",
-                    opacity: [0.9, 0.8, 0.7],
-                    fontSize: {
-                      lg: "4.5rem",
-                      md: "3.5rem",
-                      sm: "3rem",
-                      xs: "2.5rem",
-                    },
-                  }, */
-                }}
-              >
-                <span  >{value.num}</span>
-              </Box>
-            </SwiperSlide> 
-          ))}
-        </Swiper>
+          Current Selection: {clickValue} Sek/month
+        </Typography>
+        <Box mt={2} p={2} sx={{
+          display:"flex",
+          justifyContent: "end",
+          alignItems:"center",
+        }}>
+          <Button onClick={handleEdit} variant="contained">
+            Edit
+          </Button>
+        </Box>
       </Box>
+      {edit && (
+        <Box
+          sx={{
+            flexGrow: 1,
+            width: { lg: "50%", md: "50%", sm: "80%", xs: "90%" },
+          }}
+        >
+          <Swiper
+            slidesPerView={5}
+            spaceBetween={0}
+            keyboard={{
+              enabled: true,
+            }}
+            navigation={true}
+            mousewheel={true}
+          >
+            {JsonValues.map((value) => (
+              <SwiperSlide key={value.id} onClick={handleSwiperSlideClick}>
+                <Box
+                  m={5}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: {
+                      lg: "4rem",
+                      md: "3rem",
+                      sm: "2.5rem",
+                      xs: "2rem",
+                    },
+                  }}
+                >
+                  {/* {numStyle ? <span className={classes.test1}>{value.num}</span> : <span>{value.num}</span>} */}
+                  <span>{value.num}</span>
+                </Box>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <Typography variant="h4">You can swipe from above list. After select a value you need to press update button below.</Typography>
+          <Box mt={2} p={2} sx={{
+            display:"flex",
+            justifyContent: "end",
+            alignItems:"center",
+          }}>
+            <Button onClick={updateEdit} variant="contained">
+              Update
+            </Button>
+          </Box>
+        </Box>
+      )}
     </>
   );
 }
